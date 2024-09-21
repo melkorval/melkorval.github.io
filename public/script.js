0aa7f4d31213d73ce66a5c3e5a1c97b1ba5cec3c
dervalinor <dervalinor@protonmail.com>
2024-09-21 15:39:50 -0500

    botones habilitar y desabilitar

--- public/script.js
+++ public/script.js
@@ -1,5 +1,13 @@
+/*referencia de los botones por medio de un Id */
+const like_button = document.getElementById("like-button");
+const dislike_button = document.getElementById("dislike-button")
 
+/*funciones para hacer los cambios cuando en los emojis si se da like o dislike */
 function toggleLike(button) {
+    /*verifcar si tiene ya un like y agregrar la elerta liked*/
+    
+    /*hacer los repectivos cambios de emoji si tiene un liked o dejar
+    igual en caso contrario*/
     button.classList.toggle('liked');
     if (button.classList.contains('liked')) {
         button.textContent = '❤️';
@@ -18,3 +26,17 @@ function toggledisLike(button) {
 }
 
 /*una vez dado un like o dislike bloquear el otro boton*/
+
+/*boton para deshabilitar botones */
+
+/*funcion que deshabilita el boton al que no se realizo click */
+function disable_button(clickbutton, otherbutton){
+    clickbutton.disabled = false;
+    otherbutton.disabled = true;
+}
+
+/*Escuchar si se da click a los botones */
+like_button.addEventListener('click', () => disable_button(like_button, dislike_button));
+dislike_button.addEventListener('click', () => dislike_button(dislike_button, like_button));
+
+/*Hacer que el usuario solo pueda dar un solo like */
